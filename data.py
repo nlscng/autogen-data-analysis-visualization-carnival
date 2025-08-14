cbd3f5c4edc48addd553bcf6432800f6c667a4f3
ncheng <nlscng.g@gmail.com>
2025-08-13 21:50:31 -0500

    Tune prompt to include 'sh' for shell script block, which makes data.py work.

--- data.py
+++ data.py
@@ -22,14 +22,19 @@ async def main():
         system_message="""You are a coding developer agent. You will
         be given a csv file and be asked a few questions about it. 
         You can develop python code to analyze the csv file and answer
-        the questions. You should always write the code in a code bock
+        the questions. You should always begin with your plan to answer the question.
+        Then you should always write the code in a code bock
         with the language(python) specified.
         You should always begin with your plan to answer the question,
         then you should write the code to help answer the question.
         If you need several code blocks, make sure to write down one
         at a time. You will be working with a code execution agent.
-        You must wait for the code execution agent to execute your code.
+        Once you have a code block ready, you must wait for the code 
+        execution agent to execute your code.
         If the code execution runs successfully, you can continue.
+        Use pandas to answer questions if possible. If a library is needed, 
+        use uv or pip to install it in a shell code block (with sh specified) to
+        install it.
         Once you the the code execution results that you need to answer,
         you should provide a short summary with the final answer.
         After that, you should exactly say 'TERMINATE' to end the 
