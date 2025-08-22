3beb9c652e082296b529439d930acae814a968aa
ncheng <nlscng.g@gmail.com>
2025-08-21 22:04:33 -0500

    Refactoring, save messages to session, and display them.

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ from data import get_team_config, orchestrate
 import asyncio
 import os
 
+def show_message(container, one_msg):
+    with container:
+        if one_msg.startswith("Developer"):
+            with st.chat_message("ai"):
+                st.markdown(one_msg)
+        elif one_msg.startswith("CodeExecutor"):
+            with st.chat_message("CodeExecutor"):
+                st.markdown(one_msg)
+        elif one_msg.startswith("Stopping reason"):
+            with st.chat_message("user"):
+                st.markdown(one_msg)
+
 st.title("Talk with your dataset")
 
 file = st.file_uploader("Upload your CSV file", type=["csv"])
@@ -17,22 +29,21 @@ prompt = st.chat_input(
     "Ask a question about your dataset, e.g., 'What are the columns in my dataset?'"
 )
 
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
 chat_container = st.container()
+
+for one_msg in st.session_state['messages']:
+    show_message(chat_container, one_msg)
 
 if prompt:
     async def query():
         team, docker_agent = await get_team_config()
         with st.spinner("Generating response..."):
             async for one_msg in orchestrate(team, docker_agent, prompt):
-                if one_msg.startswith("Developer"):
-                    with st.chat_message("ai"):
-                        st.markdown(one_msg)
-                elif one_msg.startswith("CodeExecutor"):
-                    with st.chat_message("CodeExecutor"):
-                        st.markdown(one_msg)
-                elif one_msg.startswith("Stopping reason"):
-                    with st.chat_message("user"):
-                        st.markdown(one_msg)
+                st.session_state.messages.append(one_msg)
+                show_message(chat_container, one_msg)
                 
     asyncio.run(query())        
     # with chat_container:
